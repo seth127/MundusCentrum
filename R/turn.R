7821626e9f4fd054cfac2fefe9d3729642b7bc7e
seth127 <seth127@gmail.com>
2021-07-19 00:57:10 -0400

    feat: made move_unit() take multiple units, and added kill_unit()

--- R/turn.R
+++ R/turn.R
@@ -15,13 +15,28 @@ reconcile_player_orders <- function(game) {
 }
 
 #' Move a unit
+#' @importFrom purrr walk
 #' @export
 move_unit <- function(game, player, .u, .a, .l1, .l2) {
-  # TODO: should check if it's a legal move first. Boring...
-  .pm <- read_player_map(game, player) %>%
-    mutate(
-      loc = ifelse(unit_name == .u, .l2, loc),
-      action = ifelse(unit_name == .u, .a, action)
-    )
-   write_player_map(game, player, .pm)
+  walk(.u, ~{
+    # TODO: should check if it's a legal move first. Boring...
+    .pm <- read_player_map(game, player) %>%
+      mutate(
+        loc = ifelse(unit_name == .x, .l2, loc),
+        action = ifelse(unit_name == .x, .a, action)
+      )
+    write_player_map(game, player, .pm)
+  })
+
+}
+
+#' Kill a unit
+#' @importFrom purrr walk
+#' @export
+kill_unit <- function(game, player, .u) {
+  walk(.u, ~{
+    .pm <- read_player_map(game, player) %>%
+      filter(unit_name != .x)
+    write_player_map(game, player, .pm)
+  })
 }
