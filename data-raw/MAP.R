c95ad1ba30606f34590cec5c2376d07696106c88
seth127 <seth127@gmail.com>
2021-07-19 18:01:57 -0400

    WIP: added skeleton and A for data-raw/MAP.R

---
+++ data-raw/MAP.R
@@ -0,0 +1,55 @@
+MAP <- list(
+  A1 = list(
+    name = "Forlinden",
+    borders = c("A4")
+  ),
+  A2 = list(
+    name = "Lune Valley",
+    borders = c("A4", "A3", "B2"),
+    rivers = c("A5")
+  ),
+  A3 = list(
+    name = "Evendim Hills",
+    borders = c("A2", "A5", "D2")
+  ),
+  A4 = list(
+    name = "North Mithlond",
+    borders = c("A1", "A2"),
+    rivers = c("A5", "A6"),
+    seaports = c("E1", "B1"),
+    power = TRUE
+  ),
+  A5 = list(
+    name = "Tower Hills",
+    borders = c("A3", "A6", "A7"),
+    rivers = c("A2", "B5", "B7")
+  ),
+  A6 = list(
+    name = "South Mithlond",
+    borders = c("A5", "A8"),
+    rivers = c("A4")
+  ),
+  A7 = list(
+    name = "The Shire",
+    borders = c("A5"),
+    rivers = c("B9", "B11", "E1"),
+    power = TRUE,
+    spaceport = TRUE
+  ),
+  A8 = list(
+    name = "Marlindon",
+    borders = c("A6"),
+    rivers = c("E1"),
+    power = TRUE
+  ),
+  B1 = list(
+    name = "Forodwaith",
+    borders = c("B2", "B3", "B4", "D1", "D2"),
+    seaports = c("A4"),
+    power = TRUE
+  ),
+  B2 = list(
+    ...
+  )
+  ...
+)
