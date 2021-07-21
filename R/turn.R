f51746a6e56e95ae5e3adb00b274d8c150749c3d
seth127 <seth127@gmail.com>
2021-07-21 00:31:39 -0400

    refactor: changed move_unit() to only take .l instead of .l1 and .l2

--- R/turn.R
+++ R/turn.R
@@ -17,12 +17,12 @@ reconcile_player_orders <- function(game) {
 #' Move a unit
 #' @importFrom purrr walk
 #' @export
-move_unit <- function(game, player, .u, .a, .l1, .l2) {
+move_unit <- function(game, player, .u, .a, .l) {
   walk(.u, ~{
     # TODO: should check if it's a legal move first. Boring...
     .pm <- read_player_map(game, player) %>%
       mutate(
-        loc = ifelse(unit_name == .x, .l2, loc),
+        loc = ifelse(unit_name == .x, .l, loc),
         action = ifelse(unit_name == .x, .a, action)
       )
     write_player_map(game, player, .pm)
