2c4baa00526bac8dea34d2c9ffbe688dcaf0603c
seth127 <seth127@gmail.com>
2021-07-30 01:59:25 -0400

    dev: checking in inst/scraps.R full of helpers for starting the game and debugging etc.

---
+++ inst/scraps.R
@@ -0,0 +1,48 @@
+name = "Anno Uno"
+players = list(
+  list(
+    name = "Big Grizz",
+    team = "Space Marines",
+    units = system.file("extdata", "unit-templates", "big_grizz.csv", package = "MundusCentrum")
+  ),
+  list(
+    name = "Eric",
+    team = "Space Orcs",
+    units = system.file("extdata", "unit-templates", "eric.csv", package = "MundusCentrum")
+  ),
+  list(
+    name = "Chris",
+    team = "Tyrannids",
+    units = system.file("extdata", "unit-templates", "chris.csv", package = "MundusCentrum")
+  )
+)
+points = 2000
+
+View(
+  "big_grizz.csv" %>%
+  system.file("extdata", "unit-templates", ., package = "MundusCentrum") %>%
+  read_csv()%>%
+    mutate(unit_type = map_chr(unit_type, sanitize_name)) %>%
+    left_join(UNIT, by = "unit_type")
+)
+
+View(
+  "eric.csv" %>%
+    system.file("extdata", "unit-templates", ., package = "MundusCentrum") %>%
+    read_csv()%>%
+    mutate(unit_type = map_chr(unit_type, sanitize_name)) %>%
+    left_join(UNIT, by = "unit_type")
+)
+
+View(
+  "chris.csv" %>%
+    system.file("extdata", "unit-templates", ., package = "MundusCentrum") %>%
+    read_csv()%>%
+    mutate(unit_type = map_chr(unit_type, sanitize_name)) %>%
+    left_join(UNIT, by = "unit_type")
+)
+
+
+# get keywords joined against state of game map
+View()
+
