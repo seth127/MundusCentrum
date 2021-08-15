0215d2726a1ec819ae70f9c9d5ae2b02916a2ffc
seth127 <seth127@gmail.com>
2021-08-15 08:53:52 -0400

    fix: print was messed up

--- R/print.R
+++ R/print.R
@@ -3,17 +3,17 @@
 #' @export
 print.MC_game <- function(x, .p = NULL) {
   if (isTRUE(getOption('knitr.in.progress'))) {
-    cat(glue("### {game$name} ---------------------------\n\n"))
+    cat(glue("### {x$name} ---------------------------\n\n"))
     cat("An epic throwdown, featuring:\n\n")
-    purrr::walk(get_player_names(game), ~{
+    purrr::walk(get_player_names(x), ~{
       cat(glue("* **{.x}**\n\n"))
     })
     cat("\n")
-    print_map_df(x, .p = .p, .n = 3)
+    #print_map_df(x, .p = .p, .n = 3) # not printing map_df in Rmd
   } else {
-    cli::cli_h1(game$name)
+    cli::cli_h1(x$name)
     cat("An epic throwdown, featuring:\n")
-    cli::cat_bullet(get_player_names(game))
+    cli::cat_bullet(get_player_names(x))
     cat("\n")
     print(print_map_df(x, .p = .p, .n = 3)) # I'm kinda annoyed I have to put print() here, but it doesn't seem to work without it
   }
