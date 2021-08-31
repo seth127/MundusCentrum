1672f0e62c3def9b4ea302c8d8013058ff2ea590
seth127 <seth127@gmail.com>
2021-08-30 23:11:12 -0400

    feat: added turn to print method

--- R/print.R
+++ R/print.R
@@ -8,13 +8,13 @@ print.MC_game <- function(x, .p = NULL) {
     purrr::walk(get_player_names(x), ~{
       cat(glue("* **{.x}**\n\n"))
     })
-    cat("\n")
+    cat(glue("\n\n :: Turn {game$turn} :: \n"))
     #print_map_df(x, .p = .p, .n = 3) # not printing map_df in Rmd
   } else {
     cli::cli_h1(x$name)
     cat("An epic throwdown, featuring:\n")
     cli::cat_bullet(get_player_names(x))
-    cat("\n")
+    cat(glue("\n\n :: Turn {game$turn} :: \n"))
     print(print_map_df(x, .p = .p, .n = 3)) # I'm kinda annoyed I have to put print() here, but it doesn't seem to work without it
   }
 }
