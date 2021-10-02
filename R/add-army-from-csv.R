d0077d48163551cbcd7f3b4cc31398b96313998c
seth127 <seth127@gmail.com>
2021-10-02 14:21:30 -0400

    refactor: changing add_army_to_csv() to write to within game dir

--- R/add-army-from-csv.R
+++ R/add-army-from-csv.R
@@ -4,35 +4,32 @@
 #' @param starting_loc Map location to start units in
 #' @param player_name Optional string, if passed with filter input df with `filter(.data$player == player_name)`
 #' @export
-add_army_from_csv <- function(in_path, out_path, starting_loc, player_name = NULL) {
+add_army_from_csv <- function(in_path, game_name, player_name, starting_loc) {
   checkmate::assert_string(starting_loc)
 
-  raw_df <- read_csv(path) %>%
+  raw_df <- read_csv(in_path) %>%
     mutate(unit_type = stringr::str_replace(.data$unit_type, " ?[0-9]+[A-Za-z]*$", "")) %>%
     filter(.data$player == player_name) %>%
     select(unit_type) %>%
     mutate(loc = starting_loc)
 
+  if (nrow(raw_df) == 0) {
+    rlang::abort(paste(
+      glue("There are no rows in in {in_path} with player == '{player_name}'"),
+      paste("Choose one of:", paste(unique(read_csv(in_path)$player), collapse = ", ")),
+      sep = "\n"
+    ))
+  }
+
+  out_path <- file.path(
+    dirname(game_path(game_name)),
+    "starting-armies",
+    fs::path_ext_set(sanitize_name(player_name), "csv")
+  )
   write_csv(raw_df, out_path)
 }
 
-# add_army_from_csv(
-#   "~/Downloads/MundusCentrumUnitAttributes210729.csv",
-#   system.file("extdata", "unit-templates", "big_grizz.csv", package = "MundusCentrum"),
-#   "B10",
-#   "Moby"
-# )
-#
-# add_army_from_csv(
-#   "~/Downloads/MundusCentrumUnitAttributes210729.csv",
-#   system.file("extdata", "unit-templates", "eric.csv", package = "MundusCentrum"),
-#   "C4",
-#   "Eric"
-# )
-#
-# add_army_from_csv(
-#   "~/Downloads/MundusCentrumUnitAttributes210729.csv",
-#   system.file("extdata", "unit-templates", "chris.csv", package = "MundusCentrum"),
-#   "F2",
-#   "Chris"
-# )
+# anno_duo_armies_file <- "~/Downloads/MundusCentrumUnitAttributes210729.csv"
+# add_army_from_csv(anno_duo_armies_file, "Anno Duo", "Big Grizz", "D4")
+# add_army_from_csv(anno_duo_armies_file, "Anno Duo", "Eric", "G7")
+# add_army_from_csv(anno_duo_armies_file, "Anno Duo", "Chris", "A5")
